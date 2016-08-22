ce99398853562f4a9a73b2e830064a7142eba981
pmarshall17 <pmarshall1716@gmail.com>
2016-08-22 09:41:06 -0600

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 last_name = 'Fowles'
+middle_name = 'The Great'
 first_name = 'Jacob'
 
-full_name = first_name + ' ' + last_name
+full_name = first_name + ' ' + middle_name + '' + last_name
 
 print full_name
